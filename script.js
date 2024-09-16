1c7ea2b4abf71744ee6302635aba50da82f2a6b2
Bikash Bhagat <bikash.bhagat01@gmail.com>
2024-09-16 22:09:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 //your JS code here. If required.
-// Get the input element by its id
-const inputField = document.getElementById('fname');
+const inputField = document.getElementById("fname");
 
-// Add an event listener to trigger when the input field loses focus (onblur event)
-inputField.addEventListener('blur', function() {
-  // Convert the input value to uppercase
-  inputField.value = inputField.value.toUpperCase();
+inputField.addEventListener('blur', () => {
+	inputField.value = inputField.value.toUpperCase();
 });
